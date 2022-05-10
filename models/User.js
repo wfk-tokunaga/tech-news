7a3f03b09350077f87282dbf4b51762107360769
wfk-tokunaga <wktokunaga@gmail.com>
2022-05-10 15:10:43 -0700

    added password encryption

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 class User extends Model {}
 
@@ -32,6 +33,17 @@ User.init({
         }
     }
 }, {
+    hooks: {
+        // set up beforeCreate lifecycle "hook" functionality
+        async beforeCreate(newUserData) {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+        },
+        async beforeUpdate(updatedUserData) {
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+            return updatedUserData;
+        }
+    },
     // Table config options here
     // Passing in sequelize connection
     sequelize,
