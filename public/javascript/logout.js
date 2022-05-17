8c5d6a722f824dc6eef0529bc0e21afb5b153d6c
wfk-tokunaga <wktokunaga@gmail.com>
2022-05-17 11:46:43 -0700

    added comments

--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -6,6 +6,9 @@ async function logout() {
     });
 
     if (response.ok) {
+        // This line seems to work when I make it /login
+        // But being directed to '/' is supposed to check if there's a session and if not, 
+        // Direct you back to the login page
         document.location.replace('/');
     } else {
         alert(response.statusText);
